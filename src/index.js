ce18014cee824d7f2f3562fdcc000857ab0c4030
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-21 17:16:13 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import puppeteer from 'puppeteer'
 
 (async () => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
 
-    await page.goto('https://instagram.com')
-    await page.screenshot({ path: 'face.png' })
+    await page.goto('https://www.instagram.com/rocketseat_oficial')
 
-    await browser.close()
+    await page.evaluate(() => {
+        // Toda esta função será executada no browser.
+        // Vamos pegar todas as imagens que estão na parte de posts.
+        const NodeList = document.querySelectorAll('article img')
+
+        // Transformar n NodeList em Array.
+        const imgArray = [...NodeList]
+
+        // Transformar os Nodes (elementos html) em objetos JS
+        const list = imgArray.map(({ src }) => ({ src }))
+
+        console.log(list)
+
+        // Colocar para fora da função
+    })
+
+    // await browser.close()
 })()
