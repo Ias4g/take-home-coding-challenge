20de203662990e95a0350ff9f0b9291308d53dc4
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-21 21:11:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-import puppeteer from 'puppeteer'
 import fs from 'fs'
+import puppeteer from 'puppeteer'
 
 (async () => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
-    await page.goto('https://www.instagram.com/rocketseat_oficial')
+    await page.goto('https://storage.googleapis.com/infosimples-public/commercia/case/product.html')
 
-    const imgList = await page.evaluate(() => {
+    const txtList = await page.evaluate(() => {
         // Toda esta função será executada no browser.
-
         // Vamos pegar todas as imagens que estão na parte de posts.
-        const nodeList = document.querySelectorAll('article img')
+        const nodeList = document.querySelectorAll('h2')
 
         // Transformar n NodeList em Array.
-        const imgArray = [...nodeList]
+        const txtArray = [...nodeList]
 
         // Transformar os Nodes (elementos html) em objetos JS
-        const imgList = imgArray.map(({ src }) => ({
-            src
+        const txtList = txtArray.map(({ innerText }) => ({
+            innerText
         }))
 
+
         // Colocar para fora da função
-        return imgList
+        return txtList
     })
 
     // Escrever  os dados em um arquivo (.json)
-    fs.writeFile('file.json', JSON.stringify(imgList, null, 2), err => {
-        if (err) throw new Error('Something went wrong')
+    fs.writeFile('file.json', JSON.stringify(txtList, null, 2), err => {
+        if (err) {
+            throw new Error('Something went wrong')
+        }
+
         console.log('Well done!')
     })
 
+
     await browser.close()
+    console.log(txtList)
 })()
